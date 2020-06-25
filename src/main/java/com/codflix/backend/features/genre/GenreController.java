bbf878be132ba4fa3878fb6c349059673d1a48b0
asetif <a.amara86@gmail.com>
2020-06-25 20:26:57 +0200

    feat: apdate media-datail.html page and add fonctions on media controler, GenreController

--- src/main/java/com/codflix/backend/features/genre/GenreController.java
+++ src/main/java/com/codflix/backend/features/genre/GenreController.java
@@ -2,6 +2,7 @@ package com.codflix.backend.features.genre;
 
 import com.codflix.backend.core.Template;
 import com.codflix.backend.models.Genre;
+import com.codflix.backend.models.Media;
 import spark.Request;
 import spark.Response;
 
@@ -14,10 +15,27 @@ public class GenreController {
     private final GenreDao genreDao = new GenreDao();
 
     public String list(Request request, Response response) {
-        List<Genre> genres = genreDao.getAllGenres();
+        List<Genre> genres;
+
+        String name = request.queryParams("name");
+
+        if (name != null && !name.isEmpty()) {
+            genres = genreDao.filterGenres(name);
+        } else {
+            genres = genreDao.getAllGenres();
+        }
 
         Map<String, Object> model = new HashMap<>();
         model.put("genres", genres);
         return Template.render("genre_list.html", model);
+    }
+
+    public String detail(Request request, Response res) {
+        int id = Integer.parseInt(request.params(":id"));
+        Genre genre = genreDao.getGenreById(id);
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("genre", genre);
+        return Template.render("genre_detail.html", model);
     }
 }
--- src/main/java/com/codflix/backend/features/genre/GenreDao.java
+++ src/main/java/com/codflix/backend/features/genre/GenreDao.java
@@ -2,11 +2,10 @@ package com.codflix.backend.features.genre;
 
 import com.codflix.backend.core.Database;
 import com.codflix.backend.models.Genre;
+import com.codflix.backend.models.Media;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,5 +30,50 @@ public class GenreDao {
         }
 
         return genres;
+    }
+
+    public List<Genre> filterGenres(String term) {
+        List<Genre> genres = new ArrayList<>();
+
+        Connection connection = Database.get().getConnection();
+        try {
+            PreparedStatement st = connection.prepareStatement("SELECT * FROM genre WHERE name LIKE %?%");
+            st.setString(1, term);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                genres.add(mapToGenre(rs));
+            }
+        } catch (SQLException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return genres;
+    }
+
+    public Genre getGenreById(int id) {
+        Genre genre = null;
+
+        Connection connection = Database.get().getConnection();
+        try {
+            PreparedStatement st = connection.prepareStatement("SELECT * FROM genre WHERE id=?");
+
+            st.setInt(1, id);
+
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                genre = mapToGenre(rs);
+            }
+        } catch (SQLException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return genre;
+    }
+    private Genre mapToGenre(ResultSet rs) throws SQLException, ParseException {
+        return new Genre(
+                rs.getInt(1), // id
+                rs.getString(2) // name
+
+        );
     }
 }
